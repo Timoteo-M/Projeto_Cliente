fcfe0db80f926e22420f2d76b5421af942d007a6
Timóteo Mendonça Neto <timoteo.m@uni9.edu.br>
2021-09-25 15:37:27 -0300

    start

---
+++ Cadastro de Pessoas/Controllers/PessoaController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cadastro_de_Pessoas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cadastro_de_Pessoas.Controllers
+{
+    public class PessoaController : Controller
+    {
+        // GET: Pessoa
+        public ActionResult Index()
+        {
+            PessoaModel pModel = new PessoaModel();
+            return View(pModel.Listar());
+        }
+
+        // GET: Pessoa/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Pessoa/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pessoa/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PessoaModel pModel)
+        {
+            pModel.Salvar();
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Pessoa/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            PessoaModel pModel = new PessoaModel();
+            pModel.Editar(id);
+            return View(pModel);
+        }
+
+        // POST: Pessoa/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(PessoaModel pModel)
+        {
+            pModel.Atualizar();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Pessoa/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            PessoaModel pModel = new PessoaModel();
+            pModel.Excluir(id);
+            return RedirectToAction("Index");
+        }
+
+        // POST: Pessoa/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/PessoaModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class PessoaModel
+    {
+        public int PessoaID { get; set; }
+        public string PessoaNome { get; set; }
+        public string PessoaEndereco { get; set; }
+        public string PessoaTel { get; set; }
+        public string PessoaEmail { get; set; }
+
+        readonly string connectionString = @"Data Source=DESKTOP-HV1ONTQ;Initial Catalog=Cadastro_Pessoas;Integrated Security=True";
+
+        public void Salvar()
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+
+                SqlCommand sqlCmd = new SqlCommand("INSERT INTO Pessoa VALUES(@PessoaNome, @PessoaEndereco, @PessoaTel, @PessoaEmail)", sqlCon);
+
+                sqlCmd.Parameters.AddWithValue("@PessoaNome", PessoaNome);
+                sqlCmd.Parameters.AddWithValue("@PessoaEndereco", PessoaEndereco);
+                sqlCmd.Parameters.AddWithValue("@PessoaTel", PessoaTel);
+                sqlCmd.Parameters.AddWithValue("@PessoaEmail", PessoaEmail);
+
+                sqlCmd.ExecuteNonQuery();
+            }
+        }
+
+
+    }
+}
